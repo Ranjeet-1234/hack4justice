3064a85197e05cd4c2d809a6796bf9e07fdb4547
Ranjeet Rewatkar <99632844+Ranjeet-1234@users.noreply.github.com>
2023-10-31 23:18:18 +0530

    Add files via upload

---
+++ New-Project/app.js
@@ -0,0 +1,10 @@
+let a = prompt("Enter the valid name it only include the characters : ");
+if(a == "YashwantMule"){
+    console.log("good your name has been Recored : ");
+}else if(a == "RanjeetRewatkar"){
+    console.log("good your name has been Recored : ");
+}else if(a == "ShravanJare"){
+    console.log("good your name has been Recored : ");
+}else{
+    console.log("Enter the valid friend Name of Shivammathapati ");
+}
